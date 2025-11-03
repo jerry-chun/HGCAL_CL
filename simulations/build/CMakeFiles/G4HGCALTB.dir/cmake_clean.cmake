a1accfe7e96e6d5320fabb039f0cabd1eb9f89d6
MaxMarriottClarke <maxmarriottclarke@me.com>
2025-11-03 11:42:46 +0000

    Stop tracking build directory

--- simulations/build/CMakeFiles/G4HGCALTB.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/G4HGCALTB"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/G4HGCALTB.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
